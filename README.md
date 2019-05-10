373418342e6468908630d9f000f3d44d4999702b
fgurant <2501224066@qq.com>
2019-05-10 23:40:51 +0800

    20190510

--- README.md
+++
@@ -1,2 +0,0 @@
-# Golang-StudyNotes
-跟随@无闻视频学习Golang
---
+++ 基本结构.go
@@ -0,0 +1,25 @@
+//当前程序包名
+package main
+
+//导入其他的包
+import std "fmt"
+
+//常量定义
+const PI = 3.14
+
+//全局变量的声明与复制
+var name = "gopher"
+
+//一般类型声明
+type newType int
+
+//结构的声明
+type gopher struct{}
+
+//接口的声明
+type golang interface{}
+
+//由 main 函数作为程序的入口点启动
+func main() {
+	std.Println("Hellow World!")
+}
